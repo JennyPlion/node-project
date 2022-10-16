6c5c51aff5f41b4b105074864eae71e945cbc947
phuong bui <Phuongtracy27@gmail.com>
2022-10-16 16:21:08 +0700

    feat: setup kafka

--- micro-server/src/app.controller.ts
+++ micro-server/src/app.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('user-topic') // topic name here
+  getHello(@Payload() message) {
+    const id = message.userid;
+    // Plus operator here does string to number conversion on the fly
+    
+    return this.appService.findUserByUserId(+id);
   }
 }
